909b60a844ddbf47aa176b57410cbbbdb551024e
James Disley <dizley@googlemail.com>
2014-11-06 08:32:16 -0700

    Initial commit, showing calculation of distance using haversine method for decimal degrees locations.

---
+++ py_distance/demo.py
@@ -0,0 +1,17 @@
+'''
+Created on Nov 6, 2014
+
+@author: James Disley
+'''
+
+from py_distance import distance_calc
+
+if __name__ == '__main__':
+    point1 = (50+3/60+59/3600, -1*(5+42/60+53/3600))
+    point2 = (58+38/60+38/3600, -1*(3+4/60+12/3600))
+    
+    print(point1)
+    print(point2)
+    
+    print(distance_calc.distance_calc(point1, point2, 'haversine'))
+    
---
+++ py_distance/distance_calc.py
@@ -0,0 +1,20 @@
+'''
+Created on Nov 6, 2014
+
+@author: James Disley
+'''
+
+import py_distance.haversine as hav
+
+def distance_calc(point1, point2, calcType = 'haversine'):
+    """
+    Input:    point1    :    tuple of two floating point numbers between -180 and 180, where positive indicates North/East
+                                                                                    negative indicates South/West
+              point2    :    tuple of two floating point numbers between -180 and 180, where positive indicates North/East
+                                                                                    negative indicates South/West
+    """
+    
+    if(calcType == 'haversine'):
+        return hav.calculate(point1, point2)
+    else:
+        pass
